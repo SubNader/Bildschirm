fd68838778fa93ccf26858d967d2e8eab5b54d41
Mohamed <moh_amgad@hotmail.com>
2017-12-03 13:47:15 +0200

    Improve error handling

--- client.py
+++ client.py
@@ -13,24 +13,35 @@ class client:
 
 	# Socket creation
 	def create_socket(self):
-		connection = (self.address, self.port)
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		sock = socket.create_connection(connection)
-		sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, self.packet_size)
-		sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		return sock
+		while True:
+			try:
+				connection = (self.address, self.port)
+				sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				sock = socket.create_connection(connection)
+				sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, self.packet_size)
+				sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+				return sock
+			except:
+				print "[!] Error creating socket .... trying again"
+				time.sleep(2)
 
 	# Start client
 	def start(self):
+
+
 		sock = self.create_socket()
 		while True:
-			steam_buffer = io.BytesIO()
-			ImageGrab.grab().save(steam_buffer,'PNG')
-			screenshot = steam_buffer.getvalue()
-			#compress screenshot
-			screenshot = zlib.compress(screenshot)
-			sock.sendall(screenshot)
-			time.sleep(0.1)
+			try:
+				stream_buffer = io.BytesIO()
+				ImageGrab.grab().save(stream_buffer,'PNG')
+				screenshot = stream_buffer.getvalue()
+				#compress screenshot
+				screenshot = zlib.compress(screenshot)
+				sock.sendall(screenshot)
+				time.sleep(0.1)
+			except:
+				print "[!] Error starting client.... trying again"
+				time.sleep(2)
 
 if __name__ == '__main__':
 	elclient = client('127.0.0.1',9595)
